f032a4770ea629e9689fb0a32ca547c5e478f239
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-03-02 10:58:08 +0000

    Format JavaScript with Prettier

--- app/lib/search.js
+++ app/lib/search.js
@@ -7,24 +7,23 @@
 export const createSearchIndex = (permalink, getItems) => ({
   data: () => ({ eleventyExcludeFromCollections: true, permalink }),
   render({ collections, options }) {
-    const index = getItems(collections)
-      .map((item) => ({
-        //
-        // This logic taken directly from:
-        // https://github.com/x-govuk/govuk-eleventy-plugin/blob/4b13c204591a4afadf1af501612288e077e1ce5a/src/templates/search-index.11ty.js
-        //
-        title: item.data.title,
-        ...(item.data.description && { description: item.data.description }),
-        ...(item.data.eleventyNavigation.parent &&
-          item.data.eleventyNavigation.parent !== item.data.options.homeKey && {
+    const index = getItems(collections).map((item) => ({
+      //
+      // This logic taken directly from:
+      // https://github.com/x-govuk/govuk-eleventy-plugin/blob/4b13c204591a4afadf1af501612288e077e1ce5a/src/templates/search-index.11ty.js
+      //
+      title: item.data.title,
+      ...(item.data.description && { description: item.data.description }),
+      ...(item.data.eleventyNavigation.parent &&
+        item.data.eleventyNavigation.parent !== item.data.options.homeKey && {
           section: item.data.eleventyNavigation.parent
         }),
-        ...(item.data.date && { date: this.govukDate(item.data.date) }),
-        ...(item.url && { url: this.htmlBaseUrl(item.url, options.url) }),
-        ...(item.templateContent && {
-          tokens: this.tokenize(item.templateContent)
-        })
-      }))
+      ...(item.data.date && { date: this.govukDate(item.data.date) }),
+      ...(item.url && { url: this.htmlBaseUrl(item.url, options.url) }),
+      ...(item.templateContent && {
+        tokens: this.tokenize(item.templateContent)
+      })
+    }))
     return JSON.stringify(index, null, 2)
   }
 })
